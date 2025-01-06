b074678f428b66e9aa69862d7331aeccb37347fb
Leon Siqueira <leon.siqueir4@gmail.com>
2025-01-06 20:31:58 -0300

    feat: implement internationalization with locale support and flash alerts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,15 +6,21 @@ class ApplicationController < ActionController::Base
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  around_action :switch_locale
 
   def current_user
     Current.user
   end
 
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
   private
 
     def user_not_authorized
-      flash[:alert] = "You are not authorized to perform this action."
+      flash[:alert] = I18n.t("flash_alerts.application.not_authorized")
       redirect_to(request.referrer || root_path)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
   mount MissionControl::Jobs::Engine, at: "/jobs"
 
-  resources :posts do
-    resources :comments, only: %i[ create ]
+  scope "/:locale" do
+    resources :posts do
+      resources :comments, only: %i[ create ]
+    end
+    resources :users, only: %i[ new create update destroy]
+    get "profile" => "users#profile", as: :profile
+    get "edit_profile" => "users#edit_profile", as: :edit_profile
+    resource :session
+    resources :passwords, param: :token
+    root "posts#index"
   end
-  resources :users, only: %i[ new create update destroy]
-  get "profile" => "users#profile", as: :profile
-  get "edit_profile" => "users#edit_profile", as: :edit_profile
-  resource :session
-  resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -20,5 +23,4 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "posts#index"
 end
